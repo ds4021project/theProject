85cfd83b6e178480f91e2b879321023e44842cd8
ufufgufyg <kjfjhfjh@hjhdfhg.com>
2024-01-15 00:12:19 +0330

    add tree view

--- main/views.py
+++ main/views.py
@@ -67,15 +67,29 @@ def getPathOfTheAllFile(path) :
             elif calTheFForT == "rawFile" :
                 theResult[d] = {"type":"rawFile","forO":forOString,"href":reverse("editfile",args=[forOString])}
     return theResult
-
+def getAllDir(r) :
+    theResult = []
+    for dirname, dirnames, filenames in os.walk(theRootPathOfFiles):
+        for subdirname in dirnames:
+            theResult.append(os.path.join(dirname, subdirname).replace("\\","/").replace(theRootPathOfFiles,""))
+    fileTree = {}
+    for path in theResult:
+        parts = path.split('/')
+        node = fileTree
+        for part in parts:
+            node = node.setdefault(f"{part}", {})
+    print(fileTree)
+    return fileTree
+# {{ name|split:':_:_:'|getItem:1 }}
 def listOfFileRoot(r) :
+    
     theAllFile = getListOfFileFromFolderPath(theRootPathOfFiles)
     # print(theAllFile)
     theResult = {}
     for d in theAllFile :
         theResult[d] = {"type":"drive","forO":d,"href":reverse("listoffile",args=[d])}
     print(calCurrentPath(theRootPathOfFiles))
-    return render(r,"showFolder2.html",{"tehCurentPath":calCurrentPath(theRootPathOfFiles),"listOfFiles":theResult,"apil":r.build_absolute_uri(reverse("doSomething")),"currentPathForNew":""})
+    return render(r,"showFolder2.html",{"tehCurentPath":calCurrentPath(theRootPathOfFiles),"listOfFiles":theResult,"apil":r.build_absolute_uri(reverse("doSomething")),"currentPathForNew":"","fileTree":getAllDir(r)})
 def listOfFile(r,thePath) :
     thePath = makePathUsable(thePath)
     theAllFile = getListOfFileFromFolderPath(thePath)
@@ -84,7 +98,8 @@ def listOfFile(r,thePath) :
     currentPath = calCurrentPath(thePath)
     # print(currentPath)
     # print(theResult)
-    return render(r,"showFolder2.html",{"tehCurentPath":currentPath,"listOfFiles":theResult,"apil":r.build_absolute_uri(reverse("doSomething")),"currentPathForNew":thePath.replace(theRootPathOfFiles,"")})
+    
+    return render(r,"showFolder2.html",{"tehCurentPath":currentPath,"listOfFiles":theResult,"apil":r.build_absolute_uri(reverse("doSomething")),"currentPathForNew":thePath.replace(theRootPathOfFiles,""),"fileTree":getAllDir(r)})
 def editFile(r,thePath) :
     thePath = makePathUsable(thePath)
     # print(os.path.realpath(thePath))
